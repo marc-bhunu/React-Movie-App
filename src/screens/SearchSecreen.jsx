c3a46dc6d531c407e81727fdf927241f06e77f67
marc-bhunu <64252485+marc-bhunu@users.noreply.github.com>
2021-04-08 15:40:23 +0200

    Search Screen Works

--- src/screens/SearchSecreen.jsx
+++ src/screens/SearchSecreen.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
-import { Form, Button, FormControl, Container } from 'react-bootstrap'
-import { Header } from '../components'
+import { Form, Button, FormControl, Container, Row, Col } from 'react-bootstrap'
+import { Cards, Header } from '../components'
 import { SearchMovie } from '../services'
 
 export const SearchSecreen = () => {
@@ -12,28 +12,33 @@ export const SearchSecreen = () => {
         const data = await SearchMovie(search)
         setResult(data)
     }
-    const response = result.map(r=>(<p key={r.imdbID}>{r.Title}</p>))
-
+    
     return (
-        <>
-        <Header/>
+      <>
+        <Header />
         <Container>
-        <Form inline  className='py-3 d-flex justify-content-center' onSubmit={handleSearch} >
-            <FormControl
-                type="text"
-                placeholder="Search"
-                className="mr-sm-2"
-                value={search}
-                onChange={(event) => setSearch(event.target.value)} />
-            <Button
-                variant="outline-success"
-                onClick={handleSearch}>
-                Search
-            </Button>
-        </Form>
+          <Form
+            inline
+            className="py-3 d-flex justify-content-center"
+            onSubmit={handleSearch}>
 
-        <h1>{response}</h1>
+            <FormControl
+              type="text"
+              placeholder="Search"
+              className="mr-sm-2"
+              value={search}
+              onChange={(event) => setSearch(event.target.value)}/>
+            <Button variant="outline-success" onClick={handleSearch}>Search</Button>
+          </Form>
+          
+          <Row>
+            {result.map((r) => (
+              <Col xs="12" sm="6" lg="3" key={r.imdbID}>
+               <Cards title= {r.Title} poster={r.Poster} year={r.Year}/>
+              </Col>
+            ))}
+          </Row>
         </Container>
-    </>
-    )
+      </>
+    );
 }
